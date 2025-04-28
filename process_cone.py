98835ab0593dd4525619cc884dd50c9a9a41a9d7
Daniel <--edit>
2025-04-28 13:40:51 +0200

    Add code to process ConE scores

--- process_cone.py
+++ process_cone.py
@@ -1,35 +1,37 @@
 import os
 import pickle as p
-import numpy as np
-import matplotlib.pyplot as plt
+from tqdm import tqdm
 
-# Define the base directories
-base_dirs = {
-    "ConE": "ConE Answers",
-    "CQD": "CQD Answers",
-    "QTO": "QTO Answers"
-}
-
-# Define query structures of interest
 query_structures = ["1p", "2p", "3p", "2i", "3i", "ip", "pi"]
 
-# Dictionary to store mappings
-query_mappings = {"ConE": {}, "CQD": {}, "QTO": {}}
-
-with open(os.path.join('data', 'FB15k-237+H', 'test-easy-answers.pkl'), 'rb') as f:
-    easy_answers = p.load(f)
-
-with open(os.path.join('data', 'FB15k-237+H', 'test-hard-answers.pkl'), 'rb') as f:
-    hard_answers = p.load(f)
-
 # Process ConE
-cone_path = base_dirs["ConE"]
+cone_path = "ConE Answers"
 for dataset in os.listdir(cone_path):
+    if not dataset.startswith("ConE"):
+        continue
+
     dataset_path = os.path.join(cone_path, dataset)
-    if os.path.isdir(dataset_path):
-        for query in query_structures:
-            query_file = os.path.join(dataset_path, f"{query}.pkl")
-            with open(query_file, "rb") as f:
-                query_data = p.load(f)
-            assert next(iter(query_data)) in easy_answers
-            assert next(iter(query_data)) in hard_answers
+
+    dataset_name = dataset[5:-8]
+    print(f"Processing {dataset_name}")
+
+    with open(os.path.join('data', dataset_name, 'test-easy-answers.pkl'), 'rb') as f:
+        easy_answers = p.load(f)
+
+    with open(os.path.join('data', dataset_name, 'test-hard-answers.pkl'), 'rb') as f:
+        hard_answers = p.load(f)
+
+    for structure in tqdm(query_structures):
+        query_file = os.path.join(dataset_path, f"{structure}.pkl")
+        with open(query_file, "rb") as f:
+            query_data = p.load(f)
+
+        for query, rankings in query_data.items():
+            easy = easy_answers[query]
+            hard = hard_answers[query]
+            assert len(easy) + len(hard) == len(rankings)
+            # Get rid of rankings for easy answers
+            query_data[query] = rankings[len(easy):]
+
+        with open(os.path.join(dataset_path, f"{structure}-hard-rankings.pkl"), "wb") as f:
+            p.dump(query_data, f)
